c9d788d509fcde62e2ccd7b609b13f67ae2b86dc
Warbler14 <firefly11a@naver.com>
2019-08-23 22:00:42 +0900

    opencv 411 ->347

--- src/com/vision/main/CvMain.java
+++ src/com/vision/main/CvMain.java
@@ -4,7 +4,8 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
-import com.vision.modifyImage.ModifyImage;
+import com.vision.process.CompareImages;
+import com.vision.process.ModifyImage;
 
 public class CvMain {
 	public static void main(String[] args) {
@@ -13,7 +14,20 @@ public class CvMain {
 		Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
 		System.out.println("mat = " + mat.dump());
 		
-		new ModifyImage().modifyImageTest("test01.jpg");
-		new ModifyImage().modifyImageTest("test01.png");
+//		new ModifyImage().modifyImageTest(getResourceFilePath("test01.jpg"));
+//		new ModifyImage().modifyImageTest(getResourceFilePath("test01.png"));
+		
+		System.out.println("=========Maching test jpg=========");
+		new CompareImages().compareImages(getResourceFilePath("test01.jpg"), getResourceFilePath("test01.jpg"));
+		new CompareImages().compareImages(getResourceFilePath("test01.jpg"), getResourceFilePath("test02.jpg"));
+		new CompareImages().compareImages(getResourceFilePath("test01.jpg"), getResourceFilePath("test01-gray.jpg"));
+		
+		System.out.println("=========Maching test png=========");
+		new CompareImages().compareImages(getResourceFilePath("test01.png"), getResourceFilePath("test01.png"));
+		new CompareImages().compareImages(getResourceFilePath("test01.png"), getResourceFilePath("test01-gray.png"));
+	}
+	
+	public static String getResourceFilePath(final String fileName) {		
+		return System.getProperty("user.dir")  + "/resources/" + fileName;
 	}
 }
---
+++ src/com/vision/process/ModifyImage.java
@@ -0,0 +1,37 @@
+package com.vision.process;
+
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class ModifyImage {
+
+	public void modifyImageTest(final String location) {
+		
+		// prepare to convert a RGB image in gray scale
+		System.out.print("Convert the image at " + location + " in gray scale... ");
+		
+		// get the jpeg image from the internal resource folder
+		Mat image = Imgcodecs.imread(location);
+		
+		// convert the image in gray scale
+		Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2GRAY);
+		
+		// write the new image on disk
+		Imgcodecs.imwrite(getModifiyFileName(location, "gray"), image);
+		System.out.println("Done!");
+		
+	}
+	
+	private static String getModifiyFileName(final String filePath, final String addName) {
+		Integer extendIndex = filePath.lastIndexOf(".");
+		StringBuilder builder = new StringBuilder( filePath.substring(0, extendIndex));
+		builder.append("-").append(addName).append(".").append(filePath.substring(extendIndex + 1, filePath.length()));
+		return builder.toString();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getModifiyFileName("test.jpg", "white"));
+	}
+}
+
